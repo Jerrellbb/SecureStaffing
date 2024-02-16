f017288d04ff2bd2899daf0d0278effdf6455f07
Jerrell Boyer <jerrellbb@yahoo.co.uk>
2024-02-16 18:55:12 +0000

    added roles model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
-  pass
+  role = models.ForeignKey(
+    to='roles.Role',
+    on_delete=models.CASCADE,
+    related_name='role',
+    null=True
+  )
+  
